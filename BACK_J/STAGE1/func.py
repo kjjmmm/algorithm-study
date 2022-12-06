a4ef713f32aff34f64c51a09b44437ead4521020
kjjmmm PC <kjjmmm123@naver.com>
2022-12-06 23:50:59 +0900

    BACK start stage1 - 2022.12.06

---
+++ BACK_J/STAGE1/func.py
@@ -0,0 +1,35 @@
+def ques1():
+    print("Hello world and python algorithm");
+
+def ques2():
+    a, b = map(int,input().split())
+    print(a+b)
+
+def ques3():
+    a, b = map(int,input().split())
+    print(a-b)
+
+def ques4():
+    a, b = map(int,input().split())
+    print(a*b)
+
+def ques5():
+    a, b = map(int,input().split())
+    print(a/b)
+
+def ques6():
+    a, b = map(int,input().split())
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    print(a/b)
+    print(a%b)
+
+def ques7():
+    a = input()
+    print(a+"??!")
+
+ques7()
+
+
+
