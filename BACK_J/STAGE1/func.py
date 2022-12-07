60589cf3010f2ab85875029dd72a073312fc69ee
!rlawlals95 <kjjmmm123@naver.com>
2022-12-07 20:28:46 +0900

    stage 1 complete 2022.12.07

--- BACK_J/STAGE1/func.py
+++ BACK_J/STAGE1/func.py
@@ -29,7 +29,66 @@ def ques7():
     a = input()
     print(a+"??!")
 
-ques7()
+
+def ques8():
+    a = input()
+    print(a-543)
+
+def ques9():
+    a, b, c, d, e, f = map(int,input().split())
+    x = 1 - a
+    x2 = 1 - b
+    x3 = 2 - c 
+    x4 = 2 - d
+    x5 = 2 - e
+    x6 = 8 - f
+
+    print(x, x2, x3, x4, x5, x6)
+
+def ques10():
+    a, b, c = map(int,input().split())
+    result1 = (a+b)%c
+    result2 = ((a%c) + (b%c)) % c
+    result3 = a*b%c
+    result4 = ((a%c) * (b%c)) % c
+
+    print(result1)
+    print(result2)
+    print(result3)
+    print(result4)
+
+def ques11():
+    x = input()
+    y = input()
+
+    print(int(x)*int(y[2]))
+    print(int(x)*int(y[1]))
+    print(int(x)*int(y[0]))
+    print(int(x)*int(y))
+
+def ques12():
+    print("\\    /\\")
+    print(" )  ( ')")
+    print("(  /  )")
+    print(" \\(__)|")
+
+
+def ques13():
+    print("|\\_/|")
+    print("|q p|   /}")
+    print("( 0 )\"\"\"\\")
+    print("|\"^\"`    |")
+    print("||_/=\\\\__|")
+
+def ques14():
+    print("         ,r'\"7")
+    print("r`-_   ,'  ,/")
+    print(" \. \". L_r'")
+    print("   `~\\/")
+    print("      |")
+    print("      |")
+
+ques14()
 
 
 
