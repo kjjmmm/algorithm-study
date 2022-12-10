2566c2dbf8e2819797cfbfb6961541681b216d6a
kjjmmm PC <kjjmmm123@naver.com>
2022-12-10 11:11:34 +0900

    2022.12.10

--- BACK_J/stage3.py
+++ BACK_J/stage3.py
@@ -1,3 +1,4 @@
+
 def ques1():
     a = int(input())
     for i in range(1,10):
@@ -33,4 +34,82 @@ def ques4():
     else:
         print('No')
 
-ques4()
+
+
+def ques5():
+    import sys
+
+    n = int(sys.stdin.readline().rstrip())
+    for i in range(n):
+        a, b = map(int, sys.stdin.readline().rstrip().split())
+        print(a+b)
+
+def ques6():
+    n = int(input())
+    for i in range(n):
+        a, b = map(int,input().split())
+        print(f"Case #{i+1}: {a+b}")
+        
+
+        
+def ques7():    
+    n = int(input())
+    for i in range(n):
+        a, b = map(int, input().split())
+        print(f"Case #{i+1}: {a} + {b} = {a+b}")
+
+def ques8():
+    n = int(input())
+    for i in range(n):
+        print("*"*(i+1))
+
+
+def ques9():
+    n = int(input())
+    for i in range(n):
+        print(" "* (n-(i+1)) + "*"* (i+1) )
+
+def ques10():
+
+    while True:
+        a, b = map(int, input().split())
+        if a == 0 and b == 0 :
+            break;
+
+        print(a+b)
+
+def ques11():
+
+    while True:
+        try :
+            a, b = map(int, input().split())
+            print(a+b)
+        except EOFError:
+            break;
+
+def ques12():
+    first_input = input()
+    if int(first_input) < 10 :
+        first_input = "0"+first_input
+    save = first_input
+    cnt=0
+
+    def ques12_sub(n):
+        
+        first = n[0]
+        last = n[len(n)-1]
+        c = str(int(first) + int(last))
+        c = c[len(c)-1]
+        return(last+c)
+
+    while(True):
+        save = ques12_sub(save)
+        cnt+=1
+        if first_input==save :
+            print(cnt)
+            break
+        
+# 파이썬 함수 정의 와 변수 선언은 보통 어떻게 할까 ? 
+ques12()
+
+
