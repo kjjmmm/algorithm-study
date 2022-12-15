01ce119297b937e9de0898136bb8bb76ede3a577
!rlawlals95 <kjjmmm123@naver.com>
2022-12-15 12:40:32 +0900

    2022.12.15

--- BACK_J/stage10_정렬.py
+++ BACK_J/stage10_정렬.py
@@ -26,6 +26,39 @@ def ques3():
 
     arr.sort(reverse=True)
     print(arr[b-1])
-ques3()
+
+
+def ques4():
+    import sys
+    n = int(sys.stdin.readline())
+
+    arr=[]
+    for i in range(n):
+        arr.append(int(sys.stdin.readline()))
+    arr.sort()
+
+    for i in arr:
+        print(i)
+
+
+# 공간 복잡도 : 4mb = 100만개
+# 시간 복잡도 : 1초 = 2천만번
+# 카운팅 정렬까지 필요없고. 숫자의 범위가 1~10000 이므로 정렬에 카운트 해준다.
+def ques5():
+    import sys
+    input = sys.stdin.readline
+
+    count = [0] * 10001
+    n = int(input())
+
+    for i in range(n):
+        count[int(input())] += 1
+
+    for i in range(len(count)):
+        for _ in range(count[i]):
+            print(i)   
+
+
+ques5()
     
     
