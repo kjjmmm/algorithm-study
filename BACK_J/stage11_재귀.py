e738513eb67ebe19417a5af822971b530ee42200
kjjmmm PC <kjjmmm123@naver.com>
2022-12-17 19:16:45 +0900

    2022.12.17

---
+++ BACK_J/stage11_재귀.py
@@ -0,0 +1,13 @@
+def ques1():
+    import sys
+    input = sys.stdin.readline
+    
+    n = int(input())
+
+    result=1
+    for i in range(2,n+1):
+        result*=i
+    print(result)
+
+
+ques1()
