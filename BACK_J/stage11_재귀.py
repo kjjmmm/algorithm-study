da6792dd8b694c65741e6c80a865eff532478745
kjjmmm PC <kjjmmm123@naver.com>
2022-12-18 13:17:59 +0900

    2022.12.18

--- BACK_J/stage11_재귀.py
+++ BACK_J/stage11_재귀.py
@@ -10,4 +10,32 @@ def ques1():
     print(result)
 
 
-ques1()
+def ques2():
+
+    arr=[0,1]
+    for _ in range(19):
+        arr.append(arr[-1]+arr[-2])
+    
+    print(arr[int(input())])
+
+def ques3():
+
+    def recursion(s, l, r,cnt):
+        cnt+=1
+        if l >= r :
+            return [1,cnt];
+        elif s[l] != s[r] :
+            return [0,cnt]
+        else :
+            return recursion(s, l+1, r-1,cnt)
+
+    def isPalindrome(s,cnt):
+        return recursion(s, 0, len(s)-1,cnt)
+
+    n = int(input())
+    for i in range(n):
+        cnt = 0
+        s = input()
+        arr  = isPalindrome(s,cnt)
+        print(' '.join(list(map(str, arr))))
+ques3()
