f842e5e2c742d7a0ae4bc332254b7aa8b99c7271
kjjmmm PC <kjjmmm123@naver.com>
2022-12-19 22:48:32 +0900

    2022.12.19

--- BACK_J/stage12_브루트 포스.py
+++ BACK_J/stage12_브루트 포스.py
@@ -1,6 +1,6 @@
 def ques1():
     # 순열 조합 import
-    from itertools import permutations, combinations
+    from itertools import combinations
     import sys
     input = sys.stdin.readline
     
@@ -8,13 +8,17 @@ def ques1():
     arr = list(map(int, input().split()))
 
     com_arr = list(combinations(arr, 3))
+
+    for i in range(len(com_arr)):
+        com_arr[i]=sum(com_arr[i])
     
     com_arr.sort()
+    
     result_num=0
     for i in range(len(com_arr)):
-        if sum(com_arr[i]) > b : 
+        if com_arr[i] > b : 
             break
-        result_num=sum(com_arr[i])
+        result_num=com_arr[i]
     
     print(result_num)
 ques1()
