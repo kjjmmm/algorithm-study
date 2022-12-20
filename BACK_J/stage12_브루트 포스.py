fca2df341f0d6f68bbb90da85851f6cd9081adc2
kjjmmm PC <kjjmmm123@naver.com>
2022-12-20 22:29:47 +0900

    2022.12.20

--- BACK_J/stage12_브루트 포스.py
+++ BACK_J/stage12_브루트 포스.py
@@ -21,7 +21,31 @@ def ques1():
         result_num=com_arr[i]
     
     print(result_num)
-ques1()
+
+
+def ques2():
+    import sys
+    input = sys.stdin.readline
+
+    def ques2_sub(n):
+        str_n = str(n)
+
+        result = n
+        for i in range(len(str_n)):
+            result += int(str_n[i])
+        return result
+
+    n = int(input())
+
+    for i in range(1,10000001):
+        if n <= i :
+            print(0)
+            break;
+        if ques2_sub(i)==n :
+            print(i)
+            break;
+
+
 
 
 
