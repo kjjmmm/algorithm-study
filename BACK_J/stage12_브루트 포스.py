26212602de69ff61cb1e1c7ac1f9d6f7338cdbe9
!rlawlals95 <kjjmmm123@naver.com>
2022-12-21 17:43:32 +0900

    2022.12.21

--- BACK_J/stage12_브루트 포스.py
+++ BACK_J/stage12_브루트 포스.py
@@ -21,7 +21,45 @@ def ques1():
         result_num=com_arr[i]
     
     print(result_num)
-ques1()
 
+def ques2():
+    import sys
+    input = sys.stdin.readline
+
+    def ques2_sub(n):
+        str_n = str(n)
+
+        result = n
+        for i in range(len(str_n)):
+            result += int(str_n[i])
+        return result
+
+    n = int(input())
+
+    for i in range(1,10000001):
+        if n <= i :
+           print(0)
+           break;
+        if ques2_sub(i)==n :
+           print(i)
+           break;
+def ques3():
+    import sys
+    input = sys.stdin.readline
+    n = int(input())
+
+    arr = []
+    for i in range(n):
+        arr.append(list(map(int, input().split())))
+
+    for i in range(len(arr)):
+        cnt=0
+        for j in range(len(arr)):
+            if arr[i][0] < arr[j][0] and arr[i][1] < arr[j][1]:
+                cnt+=1
+        print(cnt+1, end=" ")
+
+
+ques3()
 
 
