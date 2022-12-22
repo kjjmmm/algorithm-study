feeeeb278f84a50c72003b558f12d3ca41f28550
!rlawlals95 <kjjmmm123@naver.com>
2022-12-22 22:02:08 +0900

    2022.12.22

--- BACK_J/stage8_기본수학2.py
+++ BACK_J/stage8_기본수학2.py
@@ -15,4 +15,29 @@ def ques1():
 
     print(result)
 
-ques1()
+def ques2():
+    a = int(input())
+    b = int(input())
+
+    result = []
+    for i in range(a,b+1):
+        if i>2 and i%2==0:continue
+        cnt=0
+        for j in range(2,i):
+            if i % j == 0:
+                cnt+=1
+            if cnt>0:break
+        if cnt==0:
+            result.append(i)
+
+    if a==1 and b > 1 : del result[0]
+
+    if a==1 and b==1 : print(-1)
+
+    elif len(result)==0:
+        print(-1)
+    else:
+        print(sum(result))
+        print(result[0])
+
+ques2()
