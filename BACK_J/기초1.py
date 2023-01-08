d72ba3e77e1bfd031d9d143b60c4bb605438327f
kjjmmm <kjjmmm123@naver.com>
2023-01-08 23:08:19 +0900

    2023.01.08

--- BACK_J/기초1.py
+++ BACK_J/기초1.py
@@ -238,4 +238,19 @@ def ques10():
 
     print(result)
 
-ques10()
+def ques11():
+    import sys
+    n = int(input())
+    A = list(map(int, sys.stdin.readline().split()))
+    answer = [-1] * n
+    stack = []
+
+    stack.append(0)
+    for i in range(1, n):
+        while stack and A[stack[-1]] < A[i]:
+            answer[stack.pop()] = A[i]
+        stack.append(i)
+
+    print(*answer)
+
+ques11()
